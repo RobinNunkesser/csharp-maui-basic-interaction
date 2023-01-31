02b30641fc0e7e63ba3baf18c536ba6262aeb03d
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-01-31 14:22:24 +0100

    Refactoring

--- BasicInteraction/MainPage.xaml.cs
+++ BasicInteraction/MainPage.xaml.cs
@@ -2,13 +2,15 @@
 {
     public partial class MainPage : ContentPage
     {
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        void Button_Clicked(System.Object sender, System.EventArgs e)
+        void Button_Clicked(
+            System.Object sender,
+            System.EventArgs e
+        )
         {
             TermLabel.Text = TermEntry.Text.ToUpper();
         }
--- BasicInteraction/Platforms/MacCatalyst/AppDelegate.cs
+++ BasicInteraction/Platforms/MacCatalyst/AppDelegate.cs
@@ -5,5 +5,6 @@ namespace BasicInteraction;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() =>
+        MauiProgram.CreateMauiApp();
 }
